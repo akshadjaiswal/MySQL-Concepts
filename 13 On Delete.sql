5a89760d2fe33a200a545de4adfb8ca26d39d7d6
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-12 14:55:45 +0530

    initial commit

---
+++ 13 On Delete.sql
@@ -0,0 +1,11 @@
+-- ON DELETE SET NULL is used if any of the value is deleted from foreign key then it will show the NULL as a value
+-- ON DELETE SET NULL
+DELETE FROM employee
+WHERE emp_id=102;
+SELECT * FROM employee;
+
+--ON DELETE CASACADE will delete the entire row
+--ON DELETE CASCADE
+DELETE FROM branch
+WHERE branch_id=2;
+SELECT * FROM branch_supplier;
