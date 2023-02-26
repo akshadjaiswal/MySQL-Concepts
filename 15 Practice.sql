be7fbf32cb39001d9374563c967edcf133b550cc
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-02-26 19:48:02 +0530

    practice query

--- 15 Practice.sql
+++ 15 Practice.sql
@@ -35,3 +35,9 @@ CREATE TABLE Persons (
 );
 
 SELECT studentID, FullName, sat_score, rcd_updated FROM student;
+
+STUDENT studentID, FullName, sat_score, recordUpdated
+FROM student
+WHERE (studentID BETWEEN 1 AND 5 OR studentID = 8)
+        AND
+        sat_score NOT IN (1000, 1400);
