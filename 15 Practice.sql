0fc9a9138703a9736586c9c23589fdf907a18a7f
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-24 19:17:29 +0530

    Update

--- 15 Practice.sql
+++ 15 Practice.sql
@@ -38,6 +38,6 @@ SELECT studentID, FullName, sat_score, rcd_updated FROM student;
 
 STUDENT studentID, FullName, sat_score, recordUpdated
 FROM student
-WHERE (studentID BETWEEN 1 AND 5 OR studentID = 8)
+WHERE (studentID BETWEEN 1 AND 5 OR studentID = 7)
         AND
         sat_score NOT IN (1000, 1400);
